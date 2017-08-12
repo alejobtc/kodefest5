101d3ba2caccb264c81c4b49f8b2f37c728a6823
Anderlink234@gmail.com <Links234>
2017-08-12 01:02:04 -0500

    Base de la demo

--- app.js
+++ app.js
@@ -51,3 +51,11 @@ app.get("/", function (req, res) {
         res.end();
     });
 });
+
+
+app.set('port', process.env.PORT || 443);
+
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
