5b863498b5a7ca2e88dd5359f3fc260b77d5f5f6
Anderlink234@gmail.com <Links234>
2017-08-12 00:56:09 -0500

    Base de la demo

--- app.js
+++ app.js
@@ -1,14 +1,53 @@
-var http = require('http'),
-    fs = require('fs');
+//
+//-------------------------------------APP----------------------------------
+//
+var express=require("express");
+var app=express();
+var fs=require('fs');
 
+//
 
-fs.readFile('./views/index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(3000);
+//----------------------------------SECURITY--------------------------------
+//
+app.disable('x-powered-by');
+//var helmet = require('helmet');
+//app.use(helmet());
+//
+//---------------------------------BODY-PARSER------------------------------
+//
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+//
+//-------------------------
+
+// ------------DATA BASE---------------------------------
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://rdcxzzqhactsqs:fdf3dfda20cbce7a857d073ac624462b1a53b24ac262e5efea6dd68e1c03b3d8@ec2-54-221-244-196.compute-1.amazonaws.com:5432/d7elf6revnmp3n';
+var client = new pg.Client(connectionString);
+//client.connect();
+//-----------------------------------ROUTES---------------------------------
+app.get("/", function (req, res) {
+    fs.readFile('./views/index.html', function (err,html) {
+        res.write(html);
+        res.end();
+    });
 });
