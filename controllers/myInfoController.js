5c269b327d8ac33ad73acf52dc767d44ba21f221
Agustin <agustincharry@gmail.com>
2017-08-13 16:39:15 -0500

    make it better

--- controllers/myInfoController.js
+++ controllers/myInfoController.js
@@ -40,7 +40,8 @@ telegramBot.on('/myInfo', msg => {
 	        
 		        for (let i=0; i<rows.length; i++ ){
 		            mensaje += 'Empresa: '+rows[i].nombreempresa+'\n';
-		            mensaje += 'Cantidad De Acciones: '+rows[i].cantidad;
+		            mensaje += 'Cantidad De Acciones: '+rows[i].cantidad+'\n';
+		            mensaje += 'Precio Por Acción: '+rows[i].precioaccion;
 		            if(i+1<rows.length){
 		                mensaje += '\n______\n';
 		            }
