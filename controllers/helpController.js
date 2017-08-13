460df530f76af79c46c90f6d3f85e92e4477b228
Agustin <agustincharry@gmail.com>
2017-08-13 16:27:38 -0500

    make it better

--- controllers/helpController.js
+++ controllers/helpController.js
@@ -10,17 +10,17 @@ telegramBot.on('/help', msg => {
     , {resize: true});
     return telegramBot.sendMessage(
         msg.from.id, 
-        "Bienvenido a BantelegramBot que desea hacer:\b\n"+ 
+        "Bienvenido a BantelegramBot que desea hacer:\n\n"+ 
         "Si ha iniciado sesión puede acceder a los siguientes comandos:\n"+
         "/logout para salir de su cuenta \n"+
-        "/myInfo para obtener toda la informacion de su cuenta"+
+        "/myInfo para obtener toda la informacion de su cuenta\n"+
         "/transfer para consignar dinero a otras cuentas\n"+
         "/history para mirar el historial de transferencias\n"+
         "/pay para ingresar dinero a la cuenta\n\n"+
-        "Para el mercado accionario estan los siguientes comandos: \n \n"+
-        "/buyAction ver y comprar las acciones que disponga el banco"+
+        "Para el mercado accionario estan los siguientes comandos: \n"+
+        "/buyAction ver y comprar las acciones que disponga el banco\n"+
         "/sellAction vender al valor actual las acciones disponibles\n\n"+
-        "Comandos inciales: \n"+
+        "Comandos inciales:\n"+
         "/registro Para crear un nuevo usuario \n"+
         "/login para ingresar a su cuenta\n\n",  {replyMarkup}
     );
